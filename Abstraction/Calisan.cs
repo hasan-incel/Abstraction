4e1228cbde9841a8236f65b52db3de32f1368f55
Hasan İncel <h_incel@hotmail.com>
2024-08-11 19:41:08 +0300

    Add project files.

---
+++ Abstraction/Calisan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Abstraction
+{
+    // Soyut temel sınıf
+    public abstract class Calisan
+    {
+        // Ortak özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        // Soyut metot, her alt sınıf bu metodu kendi görevine göre uygulayacak
+        public abstract void Gorev();
+        public void Bilgiler()
+        {
+            Console.WriteLine("\nAdı: " + Ad);
+            Console.WriteLine("Soyadı: " + Soyad);
+            Console.WriteLine("Departman: " +Departman);
+        }
+    }
+
+    // Yazılım Geliştirici sınıfı
+    public class YazilimGelistirici : Calisan
+    {
+        public override void Gorev()
+        {
+            Bilgiler();
+            Console.WriteLine("Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+
+    // Proje Yöneticisi sınıfı
+    public class ProjeYoneticisi : Calisan
+    {
+        public override void Gorev()
+        {
+            Bilgiler();
+            Console.WriteLine("Proje Yöneticisi olarak çalışıyorum.");
+        }
+    }
+
+    // Satış Temsilcisi sınıfı
+    public class SatisTemsilcisi : Calisan
+    {
+        public override void Gorev()
+        {
+            Bilgiler();
+            Console.WriteLine("Satış Temsilcisi olarak çalışıyorum.");
+        }
+    }
+
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Abstraction;
+
+//Çalışan nesneleri
+Calisan yazilimGelistirici = new YazilimGelistirici
+{
+    Ad = "Ali",
+    Soyad = "Deli",
+    Departman = "Yazılım"
+};
+
+Calisan projeYoneticisi = new ProjeYoneticisi
+{
+    Ad = "Hasan",
+    Soyad = "Soysal",
+    Departman = "Proje Yönetimi"
+};
+
+Calisan satisTemsilcisi = new SatisTemsilcisi
+{
+    Ad = "Ayşe",
+    Soyad = "Yılmaz",
+    Departman = "Satış"
+};
+
+// Görevleri yazdır
+yazilimGelistirici.Gorev();
+projeYoneticisi.Gorev();
+satisTemsilcisi.Gorev();
